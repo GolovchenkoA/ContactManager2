bfd7c8c587a718154b62016e1446c4d9c6fff55e
GolovchenkoA <golovchenko84@mail.ru>
2016-02-07 19:18:45 +0200

    7.02 19:17
    В файле конфига datasource.properties не до конца прописана jdbc.url
    и в файле конфига спринга не получилось прописать путь к  datasource.properties

--- model/src/main/java/artem/golovchenko/model/entity/Author.java
+++ model/src/main/java/artem/golovchenko/model/entity/Author.java
@@ -7,4 +7,38 @@ import javax.persistence.*;
 
 @Entity
 public class Author {
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public Author() {
+    }
+
+    public Author(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
--- model/src/main/java/artem/golovchenko/model/entity/Book.java
+++ model/src/main/java/artem/golovchenko/model/entity/Book.java
@@ -1,7 +1,43 @@
 package artem.golovchenko.model.entity;
 
+import java.util.List;
+
 /**
  * Created by головченко on 07.02.2016.
  */
 public class Book {
+    private Long id;
+    private String title;
+    private List<Author> authors;
+
+/*    public Book() {
+    }
+
+    public Book(Long id) {
+        this.id = id;
+    }*/
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<Author> authors) {
+        this.authors = authors;
+    }
 }
