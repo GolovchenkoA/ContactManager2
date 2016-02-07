e1f5f0e49b5376c0ce5782a01031429c8e35b5a0
GolovchenkoA <golovchenko84@mail.ru>
2016-02-07 16:48:25 +0200

    Initial commit

---
+++ model/src/main/java/artem/golovchenko/model/entity/Author.java
@@ -0,0 +1,10 @@
+package artem.golovchenko.model.entity;
+
+/**
+ * Created by головченко on 07.02.2016.
+ */
+import javax.persistence.*;
+
+@Entity
+public class Author {
+}
---
+++ model/src/main/java/artem/golovchenko/model/entity/Book.java
@@ -0,0 +1,7 @@
+package artem.golovchenko.model.entity;
+
+/**
+ * Created by головченко on 07.02.2016.
+ */
+public class Book {
+}
